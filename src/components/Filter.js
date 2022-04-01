61111f4079c93f1f65479668f673c002ef602e65
Emily <emilybribragg@gmail.com>
2022-04-01 12:38:06 -0400

    commit tree 2a487dc2a84a5dd548ada9670746352907dee472

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,16 @@
 import React from "react";
+//goal: filter so that only items with names that match the text are included.
 
-function Filter({ onCategoryChange }) {
+//step 1: useState should be in shopping list; then put into input field as value (matchingItem)
+//step 2: add onSearchChange CB function to set state when input changes
+function Filter({ onCategoryChange, onSearchChange, search }) {
+  function handleSearchChange(event) {
+    onSearchChange(event.target.value)
+  }
+
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input type="text" name="search" placeholder="Search..." value={search} onChange={handleSearchChange} />
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
